b7e7bfafe13c8ffb0ccd6f9a68f4a5b770aa52b2
Sara Sultan Alharthi <salharthi0340@stu.kau.edu.sa>
2024-09-01 23:38:53 +0300

    HI

---
+++ src/saraalharthi1909093/SaraAlharthi1909093.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saraalharthi1909093;
+
+/**
+ *
+ * @author mac
+ */
+public class SaraAlharthi1909093 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(" Sara Sultan Alharthi ");
+        System.out.println(" 1909093 ");
+        System.out.println(" CPIT-251 ");
+        System.out.println(" A2 ");
+        System.out.println(" Lab2-Introduction to Gitub ");
+        
+    }
+    
+}
